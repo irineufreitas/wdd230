a85b307770c54018b1e02e3213cc16ad06615dee
irineufreitas <freitaschoro@gmail.com>
2024-03-20 07:33:12 +0100

    dark

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,12 @@ hamburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     hamburgerElement.classList.toggle('open')
 });
+
+
+const darkFeature = document.querySelector('#darkMode');
+const main = document.querySelector('main');
+
+
+darkFeature.addEventListener('click', ()=> {
+    main.classList.toggle('dark');
+});
