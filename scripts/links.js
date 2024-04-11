082e4a8fc101c2e66a8f7c5c8a43918a6e699c13
irineufreitas <freitaschoro@gmail.com>
2024-04-11 05:17:08 +0200

    test

--- scripts/links.js
+++ scripts/links.js
@@ -4,4 +4,28 @@ const linksURL = "https://irineufreitas.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
+    displayLinks(data);
+}
+
+getLinks();
+
+function displayLinks(weeks) {
+    const linksContainer = document.querySelector('#links-container');
+
+    // Loop through each week
+    weeks.forEach(week => {
+        const weekHeader = document.createElement('h2');
+        weekHeader.textContent = week.week;
+        linksContainer.appendChild(weekHeader);
+
+        // Loop through each link in the week
+        week.links.forEach(link => {
+            const linkElement = document.createElement('a');
+            linkElement.href = baseURL + link.url;
+            linkElement.textContent = link.title;
+            const listItem = document.createElement('li');
+            listItem.appendChild(linkElement);
+            linksContainer.appendChild(listItem);
+        });
+    });
 }
